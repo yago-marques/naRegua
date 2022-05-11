7c68bc7fc6c7a2223bd51518f80ea55fb7459d8c
Yago Marques <yagovictormarques@gmail.com>
2022-05-11 08:41:33 -0300

    Initial Commit

---
+++ onRegua/onReguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  onReguaApp.swift
+//  onRegua
+//
+//  Created by Yago Marques on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct onReguaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
